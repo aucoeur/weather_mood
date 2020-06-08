565270d5715f0fb5fab2446febe9faa4cc016658
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-06-08 01:56:01 -0700

    Changed MONGODB_URI

--- app.js
+++ app.js
@@ -11,18 +11,19 @@ assert = require("assert");
 const url = process.env.MONGODB_URI;
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  url,
-  { useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useUnifiedTopology: true },
-  (err, db) => {
-    assert.equal(null, err);
-    console.log("Connected successfully to database");
-
-    // db.close(); // turn on for testing
-  }
-);
+        url, {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+            useUnifiedTopology: true
+        }).then(function () {
+        console.log('MongoDB is connected');
+    })
+    .catch(function (err) {
+        console.log(err);
+        // db.close(); // turn on for testing
+    });
+    
 mongoose.connection.on("error", console.error.bind(console, "MongoDB connection Error:"));
 mongoose.set("debug", true);
 
@@ -34,7 +35,9 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -48,21 +51,30 @@ app.post('/', (req, res) => {
 
     request(link, function (err, response, body) {
         if (err) {
-            res.render('index', {location: null, error: 'Uh oh, something didn\'t happen.  Please try again.'});
+            res.render('index', {
+                location: null,
+                error: 'Uh oh, something didn\'t happen.  Please try again.'
+            });
         } else {
             let location = JSON.parse(body)
             if (location.main == undefined) {
-                res.render('home', {location: null, error: 'Uh oh, something didn\'t happen.  Please try again.'});
+                res.render('home', {
+                    location: null,
+                    error: 'Uh oh, something didn\'t happen.  Please try again.'
+                });
             } else {
                 let weatherText = `It's ${location.main.temp} °F in ${location.name}.<br /><br />You're feeling ${mood}.`
-                res.render('home', {location: weatherText, error: null})
+                res.render('home', {
+                    location: weatherText,
+                    error: null
+                })
             }
             const weatherMood = new WeatherMood({
                 added: Date(),
                 location: location.name,
                 temperature: location.main.temp,
                 mood: mood
-             })
+            })
 
             weatherMood.save();
 
@@ -70,7 +82,7 @@ app.post('/', (req, res) => {
 
         }
     })
-    
+
 })
 
 app.listen(3000, () => {
